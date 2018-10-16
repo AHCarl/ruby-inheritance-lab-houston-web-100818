26fedbaad78322ebf4a73579c8452a962751787b
Alexander Carlin <ahcarlin@gmail.com>
2018-10-16 16:07:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,3 @@
 class Student < User 
-  attr_accessor :knowledge, :first_name, :last_name
   
-  def initialize()
-    super
-    @knowledge = []
-  end
-  
-  def learn(fact)
-    @knowledge << fact
-  end
-  
-  def knowledge
-    @knowledge
-  end 
-
 end
